ff1bef1c260217c7f732dcd7d725022d4c79ff32
donegaan <donegaan@tcd.ie>
2018-10-12 16:33:15 +0100

    Got simple layout and button to make new blog working

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,10 @@
 import Web.Scotty
 
 import qualified Web.Scotty as S
-import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5 as B
 import qualified Text.Blaze.Html5.Attributes as A
 import qualified Text.Blaze.Html.Renderer.Text as R
+import Control.Monad.IO.Class (liftIO)
 
 import Data.Text.Lazy
 
@@ -13,40 +14,33 @@ main = scotty 3000 $ do
   get "/" homePage
 
   get "/newblog" newBlogPage
---   get (literal "/greet/") $ do -- literal matches the exact url string
---       html $ "Oh, wow!"
 
---   get "/greet/:name" $ do
---       name <- param "name"
---       html $ longresponse name
-
--- response :: Text -> Text
--- response n = do R.renderHtml $ do
---                   H.h1 ( "Hello " >> H.toHtml n)
-
--- longresponse :: Text -> Text
--- longresponse n = do
---   R.renderHtml $ do
---     H.head $ H.title "Welcome page"
---     H.body $ do
---       H.h1 "Welcome!"
---       H.p ("Welcome to my Scotty app, " >> H.toHtml n)
+-- getHead :: IO B.Html
+-- getHead= do
+--   head <- B.head $ do
+--             B.title "Andrew's blog"
+--             B.link B.! A.rel "stylesheet" B.! A.href "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+--   return head
 
 homePage :: ActionM()
 homePage = S.html $
-  R.renderHtml $ do
-    H.head $ H.title "Andrew's Blog"
-    H.body $
-      H.h1 "Hi to blog"
+    R.renderHtml $ do
+      B.head $ B.title "Andrew's Blog"
+      B.body $ do
+        B.h1 "Welcome to my blog"
+        B.form B.! A.action "/newblog" B.! A.formaction "get" $
+          B.button B.! A.type_ "submit" $ "New Blog"
+
 
 newBlogPage :: ActionM()
 newBlogPage = S.html $
   R.renderHtml $ do
-    H.head $ H.title "Login to Andrew' Blog"
-    H.body $
-      H.div H.! A.class_ "form" $
-        H.input H.! A.type_ "text"
-        -- H.button H.! A.type_ "button"
-      -- H.a "Sign in"
+    B.head $ B.title "Make a new blog"
+    B.body $ do
+      B.form B.! A.action "/" B.! A.formmethod "get" B.! A.name "blogForm" $ do
+        "Title"
+        B.input B.! A.type_ "text" B.! A.name "blogTitle"
+        B.input B.! A.type_ "submit"
+      B.textarea B.! A.form "blogForm" B.! A.id "blogForm" B.! A.style "width: 640px; height: 260px" $ ""
 
 -- newEntry :: Text
