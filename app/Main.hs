ad69ec5c8cc41754545a358ad0b3157ed18f2b41
donegaan <donegaan@tcd.ie>
2018-10-17 21:33:37 +0100

    File IO working

--- app/Main.hs
+++ app/Main.hs
@@ -8,19 +8,16 @@ import qualified Text.Blaze.Html5.Attributes as A
 import qualified Text.Blaze.Html.Renderer.Text as R
 import Control.Monad.IO.Class (liftIO)
 
-import Data.Text.Lazy
+import qualified Data.Text.Lazy as L
+import Data.Time
 
-import Data.Time.Clock
-import Data.Time.Calendar
-
-date :: IO (Integer,Int,Int) -- :: (year,month,day)
-date = getCurrentTime >>= return . toGregorian . utctDay
-
+fileName = "blogs.txt"
 
 data Post = Post { -- record datatype
   postTitle :: String,
-  postBody :: String,
-  postDate :: IO
+  postBody :: String
+  -- ,postID :: Int
+  -- ,postDate:: (Integer,Int,Int)
 } deriving (Show,Read)
 
 main = scotty 3000 $ do
@@ -30,9 +27,9 @@ main = scotty 3000 $ do
 
   post "/newentry" newEntry
 
+  get "/allblogs" allBlogs
+
   -- post request, can pick out the parameters such as title and body of blog
-
-
 
 homePage :: ActionM()
 homePage = S.html $
@@ -42,6 +39,11 @@ homePage = S.html $
         B.h1 "Welcome to my blog"
         B.form B.! A.action "/newblog" B.! A.formaction "get"  $
           B.button B.! A.type_ "submit" $ "New Blog"
+        B.form B.! A.action "/allblogs" B.! A.formaction "get"  $
+          B.button B.! A.type_ "submit" $ "All Blogs"
+        B.div $ do
+          B.h1 "Blog Title 1"
+          B.p "Blog body 1"
 
 
 newBlogPage :: ActionM()
@@ -50,24 +52,49 @@ newBlogPage = S.html $
     B.head $ B.title "Make a new blog"
     B.body $
       B.form B.! A.action "/newentry" B.! A.method "post" B.! A.name "blogForm" B.! A.id "blogForm" $ do
-        "Title"
+        "Title: "
         B.input B.! A.type_ "text" B.! A.name "blogTitle"
+        B.br
         B.textarea B.! A.form "blogForm" B.! A.name "blogBody" B.! A.style "width: 640px; height: 260px" $ ""
-        B.input B.! A.type_ "submit"
+        B.br
+        "Password: "
+        B.input B.! A.type_ "password" B.! A.name "blogPass"
+        B.button B.! A.type_ "submit" $ "Post Blog"
 
 newEntry :: ActionM()
-newEntry = do blogBody <- S.param "blogBody" :: ActionM String
-              title <- S.param "blogTitle" :: ActionM String
-              let newBlog = Post {postTitle = title, postBody = blogBody, postDate = date}
-              S.html $
-                R.renderHtml $ do
-                  B.head $ B.title $ B.toHtml title
-                  B.body $
-                    B.div $ do
-                      B.h1 $ B.toHtml title
-                      B.p $ B.toHtml date
-              -- S.redirect "/" -- Redirect to homepage to display blog posts
+newEntry = do passWord <- S.param "blogPass" :: ActionM String
+              if passWord == "pass" then do
+                blogMain <- S.param "blogBody" :: ActionM String
+                title <- S.param "blogTitle" :: ActionM String
+                --TODO: Post ID = length of list, date: getCurrentTime and then format the date into String
+                time <- liftIO getCurrentTime
+                let (y,m,d) = toGregorian $ utctDay time -- to get date for post
+                let newBlog = Post {postTitle = title, postBody = blogMain}
+                liftIO $ storeBlog newBlog
+                liftIO $ putStrLn "PASS WORKED "
+              else
+                liftIO $ putStrLn "NO PASS"
+              S.redirect "/" -- Redirect to homepage to display blog posts
+              -- let time = getCurrentTime :: IO UTCTime
+              -- liftIO $ print $ time
+
+storeBlog :: Post -> IO () -- Store newest blog in file at head of list
+storeBlog newPost = do
+  blogList <- getPosts
+  -- print blogList
+  writeFile fileName $ show $ newPost:blogList
+
+getPosts :: IO [Post] -- Get all blog posts from text file
+getPosts = do
+  contents <- readFile fileName
+  return $! (read contents :: [Post]) -- converts string contents to list of Posts
 
 
+-- TODO: display on homepage up to 5, click on post and it displays on its own page
+-- passwords for posting blog, view list of all blogs
 
-  -- S.html $ R.renderHtml $ B.h1 title
+allBlogs :: ActionM() -- Display all Blogs
+allBlogs = S.html $
+  R.renderHtml $
+    B.body $
+     B.h1 "All Blogs"
