be4eae5823a539cfcccf2005c7643fb83ea050d0
donegaan <donegaan@tcd.ie>
2018-10-17 09:39:56 +0100

    Passing params works. Working on the record for posts

--- app/Main.hs
+++ app/Main.hs
@@ -10,17 +10,29 @@ import Control.Monad.IO.Class (liftIO)
 
 import Data.Text.Lazy
 
+import Data.Time.Clock
+import Data.Time.Calendar
+
+date :: IO (Integer,Int,Int) -- :: (year,month,day)
+date = getCurrentTime >>= return . toGregorian . utctDay
+
+
+data Post = Post { -- record datatype
+  postTitle :: String,
+  postBody :: String,
+  postDate :: IO
+} deriving (Show,Read)
+
 main = scotty 3000 $ do
   get "/" homePage
 
   get "/newblog" newBlogPage
 
--- getHead :: IO B.Html
--- getHead= do
---   head <- B.head $ do
---             B.title "Andrew's blog"
---             B.link B.! A.rel "stylesheet" B.! A.href "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
---   return head
+  post "/newentry" newEntry
+
+  -- post request, can pick out the parameters such as title and body of blog
+
+
 
 homePage :: ActionM()
 homePage = S.html $
@@ -28,7 +40,7 @@ homePage = S.html $
       B.head $ B.title "Andrew's Blog"
       B.body $ do
         B.h1 "Welcome to my blog"
-        B.form B.! A.action "/newblog" B.! A.formaction "get" $
+        B.form B.! A.action "/newblog" B.! A.formaction "get"  $
           B.button B.! A.type_ "submit" $ "New Blog"
 
 
@@ -36,11 +48,26 @@ newBlogPage :: ActionM()
 newBlogPage = S.html $
   R.renderHtml $ do
     B.head $ B.title "Make a new blog"
-    B.body $ do
-      B.form B.! A.action "/" B.! A.formmethod "get" B.! A.name "blogForm" $ do
+    B.body $
+      B.form B.! A.action "/newentry" B.! A.method "post" B.! A.name "blogForm" B.! A.id "blogForm" $ do
         "Title"
         B.input B.! A.type_ "text" B.! A.name "blogTitle"
+        B.textarea B.! A.form "blogForm" B.! A.name "blogBody" B.! A.style "width: 640px; height: 260px" $ ""
         B.input B.! A.type_ "submit"
-      B.textarea B.! A.form "blogForm" B.! A.id "blogForm" B.! A.style "width: 640px; height: 260px" $ ""
 
--- newEntry :: Text
+newEntry :: ActionM()
+newEntry = do blogBody <- S.param "blogBody" :: ActionM String
+              title <- S.param "blogTitle" :: ActionM String
+              let newBlog = Post {postTitle = title, postBody = blogBody, postDate = date}
+              S.html $
+                R.renderHtml $ do
+                  B.head $ B.title $ B.toHtml title
+                  B.body $
+                    B.div $ do
+                      B.h1 $ B.toHtml title
+                      B.p $ B.toHtml date
+              -- S.redirect "/" -- Redirect to homepage to display blog posts
+
+
+
+  -- S.html $ R.renderHtml $ B.h1 title
